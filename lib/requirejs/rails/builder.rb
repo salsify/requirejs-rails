77cf93ae3d8cddcb49675d62ba170e9a90769a33
Peter Boling <peter.boling@gmail.com>
2013-02-12 02:25:28 -0500

    Let the people know what file path failed the compile

--- lib/requirejs/rails/builder.rb
+++ lib/requirejs/rails/builder.rb
@@ -6,17 +6,19 @@ require 'ostruct'
 module Requirejs::Rails
   class Builder
     # config should be an instance of Requirejs::Rails::Config
-    
+
     def initialize(config)
       @config = config
     end
-    
-    def build      
+
+    def build
       @config.tmp_dir
     end
 
     def digest_for(path)
       Rails.application.assets.file_digest(path).hexdigest
+    rescue StandardError => e
+      raise ArgumentError, "digest_for(#{path}) failed with #{e.class}: #{e.message}\n#{e.backtrace[0..4].join("\n")}"
     end
 
     def generate_rjs_driver
